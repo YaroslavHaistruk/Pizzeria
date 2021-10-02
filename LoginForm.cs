26628db4b19c651526f267bd297319587753ccae
YaroslavHaistruk <gaystruko1@gmail.com>
2021-10-02 13:17:25 +0200

    Create LoginForm.cs

---
+++ LoginForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizzeria
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\gayst\Desktop\Pizzeria\Pizzeria\DB\PData.mdf;Integrated Security=True;Connect Timeout=30;");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From Pracownik Where PracownikName = '" + textBox1.Text + "' and Haslo ='" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Sprawdź swój Login oraz Haslo");
+            }
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
