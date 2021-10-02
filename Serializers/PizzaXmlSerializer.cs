f87f59b706bc47b611da134d43b7eca685866396
YaroslavHaistruk <gaystruko1@gmail.com>
2021-10-02 13:39:20 +0200

    Create PizzaXmlSerializer.cs

---
+++ Serializers/PizzaXmlSerializer.cs
@@ -0,0 +1,30 @@
+﻿using Pizzeria.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Pizzeria.Serializers
+{
+    class PizzaXmlSerializer
+    {
+        public static void Serialize(Pizza pizza, string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Pizza));
+            TextWriter textWriter = new StreamWriter(filePath);
+            serializer.Serialize(textWriter, pizza);
+            textWriter.Close();
+        }
+        public static Pizza Deserialize(string filePath)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Pizza));
+            TextReader textReader = new StreamReader(filePath);
+            Pizza product = (Pizza)deserializer.Deserialize(textReader);
+            textReader.Close();
+            return product;
+        }
+    }
+}
